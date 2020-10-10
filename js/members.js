364623fa267d23d59cd7ac4c874ab0604d778e54
FEWD-Code-Adventures <fewd.code.adventures@gmail.com>
2020-10-10 10:01:59 +0200

    changed my name to full name

--- js/members.js
+++ js/members.js
@@ -2,11 +2,11 @@ const members = [
   //   {
   //     name: "Your Name Here",
   //     place: "Where do you live?",
-  //     country: "In what Country are you located"
+  //     country: "In what Country are you located",
   //     url: "URL to your portfolio page, github or Treehouse profile",
   //   },
   {
-    name: "Rohald",
+    name: "Rohald van Merode",
     place: "Amsterdam",
     country: "the Netherlands",
     url: "https://www.rohald.codes",
