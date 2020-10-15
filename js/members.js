87a86cf3a70c175732591db14f60f0d635967a7b
Tarik Dzambic <33522753+DzambaTz@users.noreply.github.com>
2020-10-15 12:47:41 +0200

    Added myself to members.js #hacktober (#26)
    Co-authored-by: FEWD-Code-Adventures <72523229+FEWD-Code-Adventures@users.noreply.github.com>

--- js/members.js
+++ js/members.js
@@ -48,6 +48,12 @@ const members = [
     url: "https://www.brandongormley.com",
   },
   {
+    name: "Tarik Dzambic",
+    place: "Tuzla",
+    country: "Bosnia and Herzegovina",
+    url: "https://github.com/DzambaTz",
+  },
+  {
     name: "Mahima Sharma",
     place: "Delhi",
     country: "India",
