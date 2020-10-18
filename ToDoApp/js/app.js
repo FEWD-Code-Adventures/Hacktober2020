36cc0f83678eaa5ebdb3568c3ed1f9a4753e9e39
Claire Tregunna <62562727+c-tregunna@users.noreply.github.com>
2020-10-18 16:01:02 +0100

    Adding to the toDoApp (#35)
    * Altered hover on todo button
    
    * Added a simple new project for a mobile menu, can be alter to change how it works
    
    * added compete button to added tasks and bagan adding function to move to complete list

--- ToDoApp/js/app.js
+++ ToDoApp/js/app.js
@@ -14,18 +14,23 @@ const todoInput = document.querySelector("#todo-input");
 const submitTodo = document.querySelector("#btn__create");
 const todoList = document.querySelector("#todolist");
 
+
 const createTodo = () => {
   if(todoInput.value){
     let importance = document.querySelector('#importance');
     importance = importance.options[importance.selectedIndex].value;
     console.log(importance);
     const todo = document.createElement("DIV");
+    const completeButton = document.createElement("BUTTON");
     todo.classList.add("todo");
+    completeButton.classList.add("complete__btn");
+    completeButton.textContent = "Complete";
     todo.dataset.importance = importance;
     const todoHTML = `
             <h2 class="todo__title">${todoInput.value}</h2>
       `;
     todo.innerHTML = todoHTML;
+    todo.appendChild(completeButton);
     todoInput.value = "";
     todoList.append(todo);
   }
@@ -35,3 +40,12 @@ const createTodo = () => {
 };
 
 submitTodo.addEventListener("click", createTodo);
+
+//Cannot read property 'addEventListener' of null - need to figure out why?
+//then move the chosen todo task to the completed div
+
+// const completeButton = document.querySelector(".complete__btn");
+// completeButton.addEventListener('click', () => {
+//   const completedList = document.querySelector(".completed");
+
+// })
