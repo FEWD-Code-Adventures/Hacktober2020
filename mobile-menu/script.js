bb635a8711c366aef765386622171207157fce31
adarsh <adkmr08@gmail.com>
2020-10-21 13:29:10 +0530

    newresponsivemenu

--- mobile-menu/script.js
+++ mobile-menu/script.js
@@ -1,14 +1,9 @@
-const menu = document.querySelector('.contain');
-const list = document.querySelector('.list');
 
-const topBar = document.querySelector('.top');
-const midBar = document.querySelector('.mid');
-const botBar = document.querySelector('.bot');
 
-menu.addEventListener('click', () => {
-	topBar.classList.toggle('changeTop');
-	midBar.classList.toggle('changeMid');
-	botBar.classList.toggle('changeBot');
-	
-list.classList.toggle('hidden')
-});
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+navToggle.addEventListener("click",function () {
+      
+    links.classList.toggle("show-links");
+})
